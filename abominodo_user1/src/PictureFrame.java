bb5f77ff620395ff6ef4e94594fe53554fd3b0ba
DELL <DELL@DELL>
2022-04-14 14:00:20 +0530

    Removed unnecessary comments in the sourcecode

--- abominodo_user1/src/PictureFrame.java
+++ abominodo_user1/src/PictureFrame.java
@@ -89,16 +89,6 @@ public class PictureFrame {
       g.setColor(Color.YELLOW);
       g.fillRect(0, 0, getWidth(), getHeight());
 
-      // numbaz(g);
-      //
-      // if (master!=null && master.orig != null) {
-      // drawRoll(g, master.orig);
-      // }
-      // if (reroll != null) {
-      // drawReroll(g, reroll);
-      // }
-      //
-      // drawGrid(g);
       if (master.mode == 1) {
         drawGridLines(g);
         drawHeadings(g);
